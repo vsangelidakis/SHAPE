45b7a11192df97d119d17401c03422887b4af7b5
Vasileios Angelidakis <33127472+vsangelidakis@users.noreply.github.com>
2020-08-12 02:10:25 +0300

    Tidying up

--- functions/Visualisation/plotAuxiliaryGeometries.m
+++ functions/Visualisation/plotAuxiliaryGeometries.m
@@ -1,52 +1,50 @@
 function h=plotAuxiliaryGeometries(pt,options)
-% 	pt		:		Particle_type: Original, Convex_hull etc
+% 	pt		:		Particle_type: Original, Convex_hull, Face_No_100, etc.
 %	options	:		struct with fields:
 %	- ellipsoid			:		boolean, whether to plot fitted ellipsoid
-%	- obb				:		boolean, whether to plot obb
+%	- OBB				:		boolean, whether to plot obb
 %	- inscribedSphere	:		boolean, whether to plot max inscribed sphere
 %	- boundingSphere	:		boolean, whether to plot min bounding sphere
-%	- Aabb				:		boolean, whether to plot Aabb
+%	- AABB				:		boolean, whether to plot Aabb
+
+%% FIXME: Update options
 
 	h=gcf;
 	%% FIXME: Maybe change what "h" is. Possibly provide a collection of all plots?
 	%% 
-	%% Particle
-	Fm=pt.Mesh.Surface_mesh.Faces;
-	Pm=pt.Mesh.Surface_mesh.Vertices;
 	
-	%% FIXME: Add option whether to plot the particle or not
-	patch('Faces',Fm(:,1:3),'Vertices',Pm(:,1:3) ,'FaceColor',[0.6, 0.6, 0.6], 'EdgeColor', 'none', 'FaceAlpha',0.5); axis square %,'FaceColor','red'
+% 	grid on; hold on
+% 	axis equal;
 
-	camlight;	% lighting gouraud; %phong
-
-% 	axis equal;		
-	axis vis3d equal;
-% 	set(gca,'visible','off')
-
-% 	view(45,22.5)	% view( -125, 12 );				% grain 1
-% 	view(90,20)	% view( -125, 12 );
-% 	view(95,40)	% view( -125, 12 );
-% 	view(107,40)	% view( -125, 12 );				% grain 1
-% 	view(105,14)
-% 	view(191,43)
-
-view( -125, 12 );
-
-% 	view(93,-13)					% grain 3
-% 	view(75,-5)					% grain 3
 	
-	grid on; hold on
+%% FIXME: Support subplots
+	% 		options.plotAuxiliaryGeometries.subplots=true
+
 	
+		%% Particle
+		if options.plotAuxiliaryGeometries.Surface_mesh
+	% 		plotMesh()
+			fig=plotMesh(pt,true,false,false,false);
+	% 		Fm=pt.Mesh.Surface_mesh.Faces;
+	% 		Pm=pt.Mesh.Surface_mesh.Vertices;
+	% 		patch('Faces',Fm(:,1:3),'Vertices',Pm(:,1:3) ,'FaceColor',[0.6, 0.6, 0.6], 'EdgeColor', 'none', 'FaceAlpha',0.5); axis square %,'FaceColor','red'
+		end
+	
+	grid on
+	hold on
+	axis equal;
+	view( -125, 12 );
+	
+
 	%% Fitted Ellipsoid
-	if options.ellipsoid
-% 		x=Pm(:,1);
-% 		y=Pm(:,2);
-% 		z=Pm(:,3);
-
-		v=pt.Auxiliary_geometries.Fitted_ellipsoid.v;
-		center = pt.Auxiliary_geometries.Fitted_ellipsoid.center;
-		dim    = pt.Auxiliary_geometries.Fitted_ellipsoid.dimensions;
-		rotmat  = pt.Auxiliary_geometries.Fitted_ellipsoid.rotmat;
+	if options.plotAuxiliaryGeometries.ellipsoid
+		% x=Pm(:,1);
+		% y=Pm(:,2);
+		% z=Pm(:,3);
+		v		= pt.Auxiliary_geometries.Fitted_ellipsoid.v;
+		center	= pt.Auxiliary_geometries.Fitted_ellipsoid.center;
+		dim		= pt.Auxiliary_geometries.Fitted_ellipsoid.dimensions;
+		rotmat	= pt.Auxiliary_geometries.Fitted_ellipsoid.rotmat;
 		
 		mind = center' - rotmat*(dim/0.5)';
 		maxd = center' + rotmat*(dim/0.5)';
@@ -66,12 +64,13 @@ view( -125, 12 );
 			2*v(7) *x    + 2*v(8)*y    + 2*v(9) * z;
 		p = patch( isosurface( x, y, z, Ellipsoid, -v(10) ) );
 		set( p, 'FaceColor', 'b', 'EdgeColor', 'none', 'FaceAlpha',0.5 );
-		camlight;
-		lighting gouraud; %phong
+% 		camlight;
+% 		lighting gouraud; %phong
 
 		
 		%% FIXME
 		%% Alternative code to plot ellipsoid (needs manual setting of orientation)
+		%% FIXME: Or use ellipsoid() to begin with
 % 		[x,y,z]=sphere;
 % 		radii=dim/2.;
 % 		x = x * radii(1);
@@ -84,31 +83,31 @@ view( -125, 12 );
 	end
 
 	%% OBB
-	if options.obb
+	if options.plotAuxiliaryGeometries.OBB
 		cornerpoints=pt.Auxiliary_geometries.OBB.cornerpoints;
 		plotminbox(cornerpoints, 'k')
-% 		hold on
 	end
 
+	hold on
 	%% Minimal bounding sphere
-	if options.boundingSphere
+	if options.plotAuxiliaryGeometries.boundingSphere
 		C=pt.Auxiliary_geometries.Minimal_bounding_sphere.center;
 		R=pt.Auxiliary_geometries.Minimal_bounding_sphere.radius;
 		
 		% 	H=VisualizeBoundSphere(Pm,R,C,Xb); %ha % FIXME
-
+		
 		[x,y,z]=sphere(100);
-
+		
 		x = x * R;
 		y = y * R;
 		z = z * R;
-
+		
 		surf(x+C(1),y+C(2),z+C(3),'FaceAlpha',0.5,'FaceColor','b','EdgeColor','none')
-% 		camlight
 	end
 
+	hold on
 	%% Maximal inscribed sphere
-	if options.inscribedSphere
+	if options.plotAuxiliaryGeometries.inscribedSphere
 		C=pt.Auxiliary_geometries.Maximal_inscribed_sphere.center;
 		R=pt.Auxiliary_geometries.Maximal_inscribed_sphere.radius;
 
@@ -118,11 +117,10 @@ view( -125, 12 );
 		z = z * R;
 		
 		surf(x+C(1),y+C(2),z+C(3),'FaceAlpha',0.5,'FaceColor','r','EdgeColor','none')
-% 		camlight
 	end
 	
-	%% Aabb
-	if options.Aabb
+	%% AABB
+	if options.plotAuxiliaryGeometries.AABB
 		ex=pt.Auxiliary_geometries.AABB.Extrema;
 		ce=pt.Auxiliary_geometries.AABB.Centroid;
 		dim=(ex(2,:)-ex(1,:))/2;
@@ -138,9 +136,7 @@ view( -125, 12 );
 			ce(1)-dim(1) ce(2)+dim(2) ce(3)+dim(3)	];
 		
 		plotminbox(cornerpoints, 'k')
-% 		hold on
-% 		camlight
 	end
 	
-	
+% 	camlight;	% lighting gouraud; %phong
 end
