f261e89119f457fce4a2e5101bf3f617cac7b4cf
Vasileios Angelidakis <v.angelidakis2@newcastle.ac.uk>
2022-03-03 16:14:48 +0000

    Example 1: Update description of parameters

--- examples/Example_1abc_Load_PointCloud_SurfaceMesh_Voxelated_Image.m
+++ examples/Example_1abc_Load_PointCloud_SurfaceMesh_Voxelated_Image.m
@@ -20,67 +20,74 @@ runPart=1; % 1: run Part 1: Input from point cloud/surface mesh/tetrahedral mesh
            % 2: run Part 2: Input from segmented voxelated image with multiple particles
 
 if runPart==1
-	options.convexHull=false; % Do not calculate the convex hull; the particle is already convex
-	%% PART 1: Load surface mesh from .stl file
+	%% Load surface mesh from .stl file
 	[P,F,n] = stlRead('Platonic_solids/Dodecahedron.stl');
+	options.useConvexHull=false; % Do not calculate the convex hull; e.g. if the particle is already convex or if we do not need it
 
 	%% Load surface texture. "Texture" is a struct with fields:
 		%  z: 2-D matrix with equal spacing, where the intensity/value of each pixel/element reflects the elevation of each point of the surface texture.
-		% dx: spacing (in length units) between measurements in X direction
-		% dy: spacing (in length units) between measurements in Y direction
+		% dx: spacing between measurements in X direction (in length units)
+		% dy: spacing between measurements in Y direction (in length units)
 	load('Texture.mat');
-
-	%% Case 1: Define particle providing a point cloud and using the "Crust" algorithm to tesselate the particle surface
+    
+	%% Case 1: Define particle from a point cloud, using the "Crust" algorithm to reconstruct its surface
 	p1=Particle(P,[],[],Texture,options);
 	
-	%% Case 2: Define particle providing a surface mesh
+	%% Case 2: Define particle from a surface mesh
 	p2=Particle(P,F,[],[],options); 
 
-	%% Case 3: Define particle providing a tetrahedral mesh
+	%% Case 3: Define particle from a tetrahedral mesh
 	[Ptet,Etet,Ftet]=s2m(P,F,1,10000); % Transform surface to tetrahedral mesh using iso2mesh
 	p3=Particle(Ptet,Etet(:,1:4),[],[],options);
 
 	%% Plot p1, p2, p3
 	figure()
 	subplot(2,2,1); title('Input=Point cloud')
-	plotmesh(p1.Original.Mesh.Surface_mesh.Vertices,p1.Original.Mesh.Surface_mesh.Faces,'FaceColor','g')
-	axis equal; grid on; alpha 0.8; camlight
-
+	patch(p1.Original.Mesh.Surface_mesh,'FaceColor','g')
+	axis equal; grid on; box on; alpha 0.8; view(3); camlight
+	
 	subplot(2,2,2); title('Input=Surface mesh')
-	plotmesh(p2.Original.Mesh.Surface_mesh.Vertices,p2.Original.Mesh.Surface_mesh.Faces,'FaceColor','b')
-	axis equal; grid on; alpha 0.8; camlight
+	patch(p2.Original.Mesh.Surface_mesh,'FaceColor','b')
+	axis equal; grid on; box on; alpha 0.8; view(3); camlight
 
 	subplot(2,2,3); title('Input=Tetrahedral mesh')
-	plotmesh(p3.Original.Mesh.Surface_mesh.Vertices,p3.Original.Mesh.Surface_mesh.Faces,'FaceColor','r')
-	axis equal; grid on; alpha 0.8; camlight
+	patch(p3.Original.Mesh.Surface_mesh,'FaceColor','r')
+	axis equal; grid on; box on; alpha 0.8; view(3); camlight
 
 elseif runPart==2
 	%% Load segmented voxelated image
 	load('SegmentedImage');
-	options.convexHull=false; % Do not calculate the convex hull; the particles are already convex
+	options.Auxiliary_Geometries.OBB.method='PCA_points';
+	% SegmentedImage is a struct with fields: 
+	% - img (Nx x Ny x Nz):	labeled voxelated image
+	% - voxel_size (1 x 3):	voxel size in x,y,z directions (generally equal values)
 	
-	labels=unique(SegmentedImage.img); %%
-	labels(labels==0)=[]; % Remove zero (empty) voxels from the labels list
+	options.useConvexHull=false; % Do not calculate the convex hull; the particles are already convex
+	
+	labels=unique(SegmentedImage.img); % Find unique labels of particles
+	labels(labels==0)=[]; % Remove zero value (corresponding to empty voxels) from the labels list
 	
 	% Iterate through all different particles
 	for i=1:length(labels) % TODO: This can become a parfor in a next release
-		% The "vox" struct contains the voxelated image (Nx x Ny x Nz) and the voxel size (1 x 3)
+		% The "vox" struct contains the voxelated image (Nx x Ny x Nz) and
+		% the voxel size (1 x 3), like the 'SegmentedImage' struct, but for
+		% each particle
 		vox.img=SegmentedImage.img.*(SegmentedImage.img==labels(i));  % Isolate each label (i.e. each particle)
 		vox.voxel_size=SegmentedImage.voxel_size; % Add voxel size
+		
 		p{i}=Particle([],[],vox,[],options);
 	end
 	
-	%% Plot initial voxelated image in voxel space
+	%% Plot initial voxelated image in voxel space: Reinstate this
 	figure()
-	volshow(SegmentedImage.img)       % Plot greyscale volumetric image
-% 	labelvolshow(SegmentedImage.img)  % Plot labeled volumetric image
+	volshow(SegmentedImage.img);		% Plot greyscale volumetric image
+% 	labelvolshow(SegmentedImage.img);	% Plot labeled volumetric image
 	
 	%% Plot surface meshes in Cartesian space
 	figure()
 	patch(p{1}.Original.Mesh.Surface_mesh,'FaceColor','g','EdgeColor','none')
 	patch(p{2}.Original.Mesh.Surface_mesh,'FaceColor','b','EdgeColor','none')
-	axis equal; grid on; box on; view(3)
-	camlight
+	axis equal; grid on; box on; view(3); camlight
 end
 
 disp('Done!')
